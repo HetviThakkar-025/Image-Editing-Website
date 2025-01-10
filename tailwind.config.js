22016c6c8bf4e38b0246d62a04491c7af29be1bf
HetviThakkar-025 <hetvithakkar.019@gmail.com>
2025-01-11 01:34:42 +0530

    added homepage designs

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./templates/**/*.{html,js}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(180deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      },
+
+      animation: {
+        'spin-slow': 'spin 7s linear infinite',
+      }
+    },
   },
   plugins: [],
 }
