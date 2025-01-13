cd3df5bacca8ec91523fe6effea0a7ddcec12957
HetviThakkar-025 <hetvithakkar.019@gmail.com>
2025-01-13 15:55:07 +0530

    tools desgin

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,18 @@ module.exports = {
         wiggle: {
           '0%, 100%': { transform: 'rotate(180deg)' },
           '50%': { transform: 'rotate(3deg)' },
-        }
+        },
+        flicker: {
+          '0%': { opacity: '1' },
+          '50%': { opacity: '0.4' },
+          '100%': { opacity: '1' },
+        },
       },
 
       animation: {
         'spin-slow': 'spin 7s linear infinite',
-      }
+        'flicker': 'flicker 1.5s infinite'
+      },
     },
   },
   plugins: [],
