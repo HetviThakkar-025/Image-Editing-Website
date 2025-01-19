fde69d4ac2f7419a0035e812c8fe936d4a4bff75
HetviThakkar-025 <hetvithakkar.019@gmail.com>
2025-01-20 01:32:05 +0530

    flip, grayscale, Rotate,morph,spaces,translate added

--- app.py
+++ app.py
@@ -30,11 +30,6 @@ def resize():
             if not img_file:
                 raise ValueError("No file uploaded")
 
-            # Check for allowed file extensions
-            allowed_extensions = {"png", "jpg", "jpeg"}
-            if img_file.filename.split('.')[-1].lower() not in allowed_extensions:
-                return jsonify({"error": "Unsupported file type"}), 400
-
             img_array = np.frombuffer(img_file.read(), np.uint8)
             img = cv.imdecode(img_array, cv.IMREAD_COLOR)
 
@@ -72,11 +67,6 @@ def blur():
             if not img_file:
                 raise ValueError("No file uploaded")
 
-            # Check for allowed file extensions
-            allowed_extensions = {"png", "jpg", "jpeg"}
-            if img_file.filename.split('.')[-1].lower() not in allowed_extensions:
-                return jsonify({"error": "Unsupported file type"}), 400
-
             img_array = np.frombuffer(img_file.read(), np.uint8)
             img = cv.imdecode(img_array, cv.IMREAD_COLOR)
 
@@ -102,6 +92,264 @@ def blur_img(img):
     blurred = cv.blur(img, (7, 7))
     return blurred
 
+
+# FLIP
+@app.route("/flip", methods=['POST'])
+def flip():
+    if request.method == 'POST':
+
+        try:
+            img_file = request.files.get("imageInput")
+            flipcode = int(request.form.get("flip"))
+
+            if not img_file:
+                raise ValueError("No file uploaded")
+
+            img_array = np.frombuffer(img_file.read(), np.uint8)
+            img = cv.imdecode(img_array, cv.IMREAD_COLOR)
+
+            if img is None:
+                return jsonify({"error": "Invalid image file"}), 400
+
+            newimg = flip_img(img, flipcode)
+
+            _, img_encoded = cv.imencode('.jpg', newimg)
+            img_bytes = img_encoded.tobytes()
+
+            img_base64 = base64.b64encode(img_bytes).decode("utf-8")
+            return jsonify({"filtered_image": img_base64}), 200
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return jsonify({"error": f"An error occurred: {str(e)}"}), 500
+
+    return render_template("editor_window.html")
+
+
+def flip_img(img, flipcode):
+    flipped = cv.flip(img, flipcode)
+    return flipped
+
+
+# GRAY
+@app.route("/gray", methods=['POST'])
+def gray():
+    if request.method == 'POST':
+
+        try:
+            img_file = request.files.get("imageInput")
+
+            if not img_file:
+                raise ValueError("No file uploaded")
+
+            img_array = np.frombuffer(img_file.read(), np.uint8)
+            img = cv.imdecode(img_array, cv.IMREAD_COLOR)
+
+            if img is None:
+                return jsonify({"error": "Invalid image file"}), 400
+
+            newimg = gray_img(img)
+
+            _, img_encoded = cv.imencode('.jpg', newimg)
+            img_bytes = img_encoded.tobytes()
+
+            img_base64 = base64.b64encode(img_bytes).decode("utf-8")
+            return jsonify({"filtered_image": img_base64}), 200
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return jsonify({"error": f"An error occurred: {str(e)}"}), 500
+
+    return render_template("editor_window.html")
+
+
+def gray_img(img):
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    return gray
+
+
+# ROTATE
+@app.route("/rotate", methods=['POST'])
+def rotate():
+    if request.method == 'POST':
+
+        try:
+            img_file = request.files.get("imageInput")
+            angle = int(request.form.get("angle"))
+
+            if not img_file:
+                raise ValueError("No file uploaded")
+
+            img_array = np.frombuffer(img_file.read(), np.uint8)
+            img = cv.imdecode(img_array, cv.IMREAD_COLOR)
+
+            if img is None:
+                return jsonify({"error": "Invalid image file"}), 400
+
+            newimg = rotate_img(img, angle)
+
+            _, img_encoded = cv.imencode('.jpg', newimg)
+            img_bytes = img_encoded.tobytes()
+
+            img_base64 = base64.b64encode(img_bytes).decode("utf-8")
+            return jsonify({"filtered_image": img_base64}), 200
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return jsonify({"error": f"An error occurred: {str(e)}"}), 500
+
+    return render_template("editor_window.html")
+
+
+def rotate_func(img, angle, point=None):
+    (height, width) = img.shape[:2]
+
+    if point is None:
+        point = (width//2, height//2)
+
+    mat = cv.getRotationMatrix2D(point, angle, 1.0)
+    dimensions = (width, height)
+
+    return cv.warpAffine(img, mat, dimensions)
+
+
+def rotate_img(img, angle):
+    rotated = rotate_func(img, angle)
+    return rotated
+
+
+# MORPH
+@app.route("/morph", methods=['POST'])
+def morph():
+    if request.method == 'POST':
+
+        try:
+            img_file = request.files.get("imageInput")
+            morph = request.form.get("morph")
+
+            if not img_file:
+                raise ValueError("No file uploaded")
+
+            img_array = np.frombuffer(img_file.read(), np.uint8)
+            img = cv.imdecode(img_array, cv.IMREAD_COLOR)
+
+            if img is None:
+                return jsonify({"error": "Invalid image file"}), 400
+
+            newimg = morph_img(img, morph)
+
+            _, img_encoded = cv.imencode('.jpg', newimg)
+            img_bytes = img_encoded.tobytes()
+
+            img_base64 = base64.b64encode(img_bytes).decode("utf-8")
+            return jsonify({"filtered_image": img_base64}), 200
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return jsonify({"error": f"An error occurred: {str(e)}"}), 500
+
+    return render_template("editor_window.html")
+
+
+def morph_img(img, m):
+    canny = cv.Canny(img, 125, 175)
+    dilate = cv.dilate(canny, (7, 7), iterations=3)
+    erode = cv.erode(dilate, (7, 7), iterations=3)
+
+    if m == "dilate":
+        return dilate
+    elif m == "erode":
+        return erode
+
+
+# MORPH
+@app.route("/spaces", methods=['POST'])
+def spaces():
+    if request.method == 'POST':
+
+        try:
+            img_file = request.files.get("imageInput")
+            space = request.form.get("space")
+
+            if not img_file:
+                raise ValueError("No file uploaded")
+
+            img_array = np.frombuffer(img_file.read(), np.uint8)
+            img = cv.imdecode(img_array, cv.IMREAD_COLOR)
+
+            if img is None:
+                return jsonify({"error": "Invalid image file"}), 400
+
+            newimg = space_img(img, space)
+
+            _, img_encoded = cv.imencode('.jpg', newimg)
+            img_bytes = img_encoded.tobytes()
+
+            img_base64 = base64.b64encode(img_bytes).decode("utf-8")
+            return jsonify({"filtered_image": img_base64}), 200
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return jsonify({"error": f"An error occurred: {str(e)}"}), 500
+
+    return render_template("editor_window.html")
+
+
+def space_img(img, s):
+    if s == "hsv":
+        hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+        return hsv
+    elif s == "lab":
+        lab = cv.cvtColor(img, cv.COLOR_BGR2LAB)
+        return lab
+
+
+# TRANSLATE
+@app.route("/translate", methods=['POST'])
+def translate():
+    if request.method == 'POST':
+
+        try:
+            img_file = request.files.get("imageInput")
+
+            try:
+                x = int(request.form.get("X"))
+                y = int(request.form.get("Y"))
+            except ValueError:
+                return jsonify({"error": "Invalid width or height"}), 400
+
+            if not img_file:
+                raise ValueError("No file uploaded")
+
+            img_array = np.frombuffer(img_file.read(), np.uint8)
+            img = cv.imdecode(img_array, cv.IMREAD_COLOR)
+
+            if img is None:
+                return jsonify({"error": "Invalid image file"}), 400
+
+            newimg = translate_img(img, x, y)
+
+            _, img_encoded = cv.imencode('.jpg', newimg)
+            img_bytes = img_encoded.tobytes()
+
+            img_base64 = base64.b64encode(img_bytes).decode("utf-8")
+            return jsonify({"filtered_image": img_base64}), 200
+
+        except Exception as e:
+            print(f"Error: {e}")
+            return jsonify({"error": f"An error occurred: {str(e)}"}), 500
+
+    return render_template("editor_window.html")
+
+
+def trans(img, x, y):
+    transMat = np.float32([[1, 0, x], [0, 1, y]])
+    dimensions = (img.shape[1], img.shape[0])
+    return cv.warpAffine(img, transMat, dimensions)
+
+def translate_img(img, x, y):
+    translated = trans(img, x, y)
+    return translated
 
 # Define and register the b64encode filter
 @app.template_filter('b64encode')
--- static/js/editor.js
+++ static/js/editor.js
@@ -12,6 +12,8 @@ const previewContainer = document.getElementById('previewContainer');
 const previewImage = document.getElementById('previewImage');
 const form = document.getElementById('imageForm');
 
+let hasClickedOnce = false;
+
 // preview image
 imageInput.addEventListener('change', (event) => {
     const file = event.target.files[0];
@@ -75,6 +77,7 @@ async function submitImageForm(endpoint, additionalData = {}) {
             imageContainer.classList.remove("hidden");
             form.classList.add("hidden");
             previewContainer.classList.add("hidden");
+            hasClickedOnce = false;
         } else {
             alert("Failed to retrieve image.");
         }
@@ -86,7 +89,6 @@ async function submitImageForm(endpoint, additionalData = {}) {
 
 
 // RESIZE
-let hasClickedOnce = false;
 const resizeButtonToolbar = document.getElementById('resizeButtonToolbar');
 const resizeButton = document.getElementById('resizeButton');
 
@@ -121,24 +123,136 @@ const blurTool = document.getElementById('blurTool');
 blurTool.addEventListener("click", async (event) => {
     event.preventDefault();
 
-    // if (!hasClickedOnce) {
-    //     document.getElementById("resize-w-h").classList.remove('hidden');
-    //     hasClickedOnce = true;
-    //     return;
-    // }
-
-    // const widthValue = document.getElementById('width').value;
-    // const heightValue = document.getElementById('height').value;
-
-    // if (!widthValue || !heightValue) {
-    //     alert("Please specify both width and height.");
-    //     return;
-    // }
-
-    // const additionalData = {
-    //     width: widthValue,
-    //     height: heightValue
-    // };
-
     submitImageForm("http://127.0.0.1:5000/blur", null);
+});
+
+// FLIP
+const flipTool = document.getElementById('flipTool');
+
+flipTool.addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    if (!hasClickedOnce) {
+        document.getElementById("flipping").classList.remove('hidden');
+        hasClickedOnce = true;
+        return;
+    }
+
+    const selectedRadio = document.querySelector('input[name="flip"]:checked');
+    const flipValue = selectedRadio ? selectedRadio.value : null;
+
+    const additionalData = {
+        flip: flipValue
+    };
+
+    console.log("Submitting with flip value:", flipValue);
+    submitImageForm("http://127.0.0.1:5000/flip", additionalData);
+});
+
+
+// GRAYSCALE
+const gray = document.getElementById('grayscale');
+
+gray.addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    submitImageForm("http://127.0.0.1:5000/gray", null);
+});
+
+
+// ROTATE
+const rotate = document.getElementById('rotate');
+
+rotate.addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    if (!hasClickedOnce) {
+        document.getElementById("rotate-a").classList.remove('hidden');
+        hasClickedOnce = true;
+        return;
+    }
+
+    const a = document.getElementById('angle').value;
+
+    const additionalData = {
+        angle: a
+    };
+
+
+    submitImageForm("http://127.0.0.1:5000/rotate", additionalData);
+});
+
+
+// MORPHOLOGY
+const morphology = document.getElementById('morphology');
+
+morphology.addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    if (!hasClickedOnce) {
+        document.getElementById("morphology-d-e").classList.remove('hidden');
+        hasClickedOnce = true;
+        return;
+    }
+
+    const selectedRadio = document.querySelector('input[name="morph"]:checked');
+    const morphValue = selectedRadio ? selectedRadio.value : null;
+
+    const additionalData = {
+        morph: morphValue
+    };
+
+    console.log("Submitting with flip value:", morphValue);
+    submitImageForm("http://127.0.0.1:5000/morph", additionalData);
+});
+
+
+// SPACES
+const spaces = document.getElementById('spaces');
+
+spaces.addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    if (!hasClickedOnce) {
+        document.getElementById("spaces-b-l").classList.remove('hidden');
+        hasClickedOnce = true;
+        return;
+    }
+
+    const selectedRadio = document.querySelector('input[name="space"]:checked');
+    const spaceValue = selectedRadio ? selectedRadio.value : null;
+
+    const additionalData = {
+        space: spaceValue
+    };
+
+    submitImageForm("http://127.0.0.1:5000/spaces", additionalData);
+});
+
+// TRANSLATE
+const translate = document.getElementById('translate');
+
+translate.addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    if (!hasClickedOnce) {
+        document.getElementById("translate-x-y").classList.remove('hidden');
+        hasClickedOnce = true;
+        return;
+    }
+
+    const x = document.getElementById('x').value;
+    const y = document.getElementById('y').value;
+
+    if (!x || !y) {
+        alert("Please specify both x and y");
+        return;
+    }
+
+    const additionalData = {
+        X: x,
+        Y: y
+    };
+
+    submitImageForm("http://127.0.0.1:5000/translate", additionalData);
 });
